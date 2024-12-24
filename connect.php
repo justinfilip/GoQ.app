2a852c40d0f09807721d3d2e11ee3ac701de7a43
Justin Filip <justin.filip@outlook.com>
2024-12-24 16:03:07 -0600

    Adding database connectors

--- connect.php
+++ connect.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
 $data = json_decode(file_get_contents('php://input'), true);
 $host = $data['host'];
