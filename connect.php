d37dec2b49aae843178a69a5dc20b9d00d194517
Justin Filip <justin.filip@outlook.com>
2024-12-24 18:00:50 -0600

    Adding database connectors

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
